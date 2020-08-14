f5a3c0f56facaab5a262a042bc0887fb8248cf1d
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-08-13 23:07:55 -0700

    added downloadable pdf of resume

--- assets/js/hover.zoom.conf.js
+++ assets/js/hover.zoom.conf.js
@@ -1,32 +1,30 @@
-        $(function() {
-        
-        	
-            $('.blue').hoverZoom({
-                overlayColor: '#3498db',
-                zoom: 0
-            });
-            
-            $('.green').hoverZoom({
-                overlayColor: '#1abc9c',
-                zoom: 0
-            });
-            
-            $('.pink').hoverZoom({
-                overlayColor: '#bd2e75',
-                zoom: 0
-            });
-            
-            $('.black').hoverZoom({
-                overlayColor: '#2f2f2f',
-                zoom: 0
-            });
-            
-            $('.alizarin').hoverZoom({
-                overlayColor: '#e74c3c',
-                zoom: 0
-            });
-            
-            /* USAGE
+$(function () {
+  $('.blue').hoverZoom({
+    overlayColor: '#3498db',
+    zoom: 1,
+  });
+
+  $('.green').hoverZoom({
+    overlayColor: '#1abc9c',
+    zoom: 0,
+  });
+
+  $('.pink').hoverZoom({
+    overlayColor: '#bd2e75',
+    zoom: 0,
+  });
+
+  $('.black').hoverZoom({
+    overlayColor: '#2f2f2f',
+    zoom: 0,
+  });
+
+  $('.alizarin').hoverZoom({
+    overlayColor: '#e74c3c',
+    zoom: 0,
+  });
+
+  /* USAGE
             
             $('#pink').hoverZoom({
                 overlay: true, // false to turn off (default true)
@@ -37,5 +35,4 @@
             });
             
             */
-            
-        }); 
+});
